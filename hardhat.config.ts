f517c749953db563915e41149f5199349ec46546
Aleksandr Pasevin <pasevin@gmail.com>
2022-01-17 18:00:40 +0300

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,71 @@
+import * as dotenv from 'dotenv';
+
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.4',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      accounts:
+        process.env.MNEMONIC !== undefined
+          ? { mnemonic: process.env.MNEMONIC }
+          : [],
+    },
+    testnet: {
+      url: process.env.MORALIS_BSC_TESTNET_ARCHIVE_URL || '',
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts:
+        process.env.SENDER001_PRIVATE_KEY !== undefined
+          ? [process.env.SENDER001_PRIVATE_KEY]
+          : [],
+    },
+    mainnet: {
+      url: process.env.MORALIS_BSC_MAINNET_URL || '',
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts:
+        process.env.SENDER001_PRIVATE_KEY !== undefined
+          ? [process.env.SENDER001_PRIVATE_KEY]
+          : [],
+    },
+    hardhat: {
+      mining: {
+        auto: false,
+      },
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || '',
+      accounts:
+        process.env.MNEMONIC !== undefined
+          ? { mnemonic: process.env.MNEMONIC }
+          : [],
+    },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  mocha: {
+    timeout: 100000,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
